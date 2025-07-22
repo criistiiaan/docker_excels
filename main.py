3b0f06383464161da254319ab40175598ff2b3d4
Cristian Arevalo <dadarevalo6@gmail.com>
2025-07-21 21:08:36 -0500

    Initial commit: Docker Python env

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import sqlalchemy
+import requests
+
+print("✅ Docker + Python environment is ready!")
+print("🧪 Pandas version:", pd.__version__)
+print("🧪 SQLAlchemy version:", sqlalchemy.__version__)
