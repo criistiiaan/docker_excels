b92f0d0f2857c8ec9298972683799e5085ab90f2
Cristian Arevalo <dadarevalo6@gmail.com>
2025-07-21 23:58:56 -0500

    Remove sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import sqlalchemy
-import requests
+
 
 print("✅ Docker + Python environment is ready!")
-print("🧪 Pandas version:", pd.__version__)
-print("🧪 SQLAlchemy version:", sqlalchemy.__version__)
